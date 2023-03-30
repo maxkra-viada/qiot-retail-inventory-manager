2d68c7a1c7f3b6ad49e8a6775bdd0421cb670d38
Maximilian Kramps <maximilian.kramps@viada.de>
2023-03-30 11:27:04 +0200

    correct js

--- src/main/resources/META-INF/resources/main.js
+++ src/main/resources/META-INF/resources/main.js
@@ -1,63 +1,58 @@
-class InventoryUI {
-  constructor() {
-    document.addEventListener('click', () => {
-      document.getElementById("createSuccess").className = "hidden";
-      document.getElementById("createFailed").className = "hidden";
+document.addEventListener('click', () => {
+  document.getElementById("createSuccess").className = "hidden";
+  document.getElementById("createFailed").className = "hidden";
+});
+
+function createProduct() {
+  let form = document.getElementById("createProduct");
+
+  let productName = form.querySelector("[name=productName]").value;
+  let productPrice = form.querySelector("[name=productPrice]").value;
+  let quantity = form.querySelector("[name=quantity]").value;
+
+  form.reset();
+
+  req = {
+    "title": productName,
+    "priceInCents": parseInt(productPrice*100),
+    "quantity": quantity,
+  }
+
+  fetch("/api/product", {
+    "method": "POST",
+    "body": JSON.stringify(req),
+    "header": {
+      'Content-Type': 'application/json'
+    },
+  }).then(() => {
+    document.getElementById("createSuccess").className = "";
+  }
+  ).catch(() => {
+    document.getElementById("createFailed").className = "";
+  });
+
+  console.log(productName, productPrice);
+
+  this.getProducts();
+}
+
+function getProducts() {
+  let list = document.getElementById("productList");
+
+  fetch("/api/product/all", {
+    "method": "GET",
+    "header": {
+      'Content-Type': 'application/json'
+    },
+  }).then((response) => {
+    response.array.forEach(element => {
+      let li = document.createElement('li');
+      li.innerHTML = element.quantity + "x " + element.title + " ("
+          + Number.parseFloat(element.priceInCents/100).toFixed(2) + "€)";
+      list.appendChild(li);
     });
   }
+  ).catch(() => {});
 
-  createProduct() {
-    let form = document.getElementById("createProduct");
-
-    let productName = form.querySelector("[name=productName]").value;
-    let productPrice = form.querySelector("[name=productPrice]").value;
-    let quantity = form.querySelector("[name=quantity]").value;
-
-    form.reset();
-
-    req = {
-      "title": productName,
-      "priceInCents": parseInt(productPrice*100),
-      "quantity": quantity,
-    }
-
-    fetch("/api/product", {
-      "method": "POST",
-      "body": JSON.stringify(req),
-      "header": {
-        'Content-Type': 'application/json'
-      },
-    }).then(() => {
-      document.getElementById("createSuccess").className = "";
-    }
-    ).catch(() => {
-      document.getElementById("createFailed").className = "";
-    });
-
-    console.log(productName, productPrice);
-
-    this.getProducts();
-  }
-
-  getProducts() {
-    let list = document.getElementById("productList");
-
-    fetch("/api/product/all", {
-      "method": "GET",
-      "header": {
-        'Content-Type': 'application/json'
-      },
-    }).then((response) => {
-      response.array.forEach(element => {
-        let li = document.createElement('li');
-        li.innerHTML = element.quantity + "x " + element.title + " ("
-            + Number.parseFloat(element.priceInCents/100).toFixed(2) + "€)";
-        list.appendChild(li);
-      });
-    }
-    ).catch(() => {});
-
-    console.log(productName, productPrice);
-  }
-
+  console.log(productName, productPrice);
 }
